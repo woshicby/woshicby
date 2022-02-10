cca23e03883e98bb3cdba4350d6a92d62edf79b6
woshicby <99337271+woshicby@users.noreply.github.com>
2022-02-10 15:07:13 +0800

    Update 1.1.7numpy安装测试.py

--- Python/机器学习实战/1.1.7numpy安装测试.py
+++ Python/机器学习实战/1.1.7numpy安装测试.py
@@ -8,5 +8,6 @@ myEye = invRandMat * randMat
 print('随机矩阵\n', randMat)
 print('随机矩阵的转置\n', randMat.T)
 print('随机矩阵的逆\n', invRandMat)
-print('矩阵与逆的乘积\n',myEye)
+print('矩阵与逆的乘积\n', myEye)
+print('单位矩阵\n', numpy.eye(4))
 print('求逆的误差\n', myEye - numpy.eye(4))
